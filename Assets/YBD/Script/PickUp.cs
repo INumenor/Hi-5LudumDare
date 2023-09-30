a5b20d5c2924008a92010d46db50b0454919003d
ygmrbrndrhm <yagmurbernaderhem@gmail.com>
2023-09-30 18:31:27 +0300

    Grab

---
+++ Assets/YBD/Script/PickUp.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    
+    public GameObject HoldSpot;
+    private GameObject ItemHolding;
+   
+   
+    private void OnCollisionStay2D(Collision2D other)
+    {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("E");
+            ItemHolding = other.gameObject;
+            ItemHolding.transform.position = HoldSpot.transform.position;
+            ItemHolding.transform.parent = HoldSpot.transform;
+            ItemHolding.GetComponent<SpriteRenderer>().sortingOrder +=1 ;
+            if (ItemHolding.GetComponent<Rigidbody2D>())
+            {
+                ItemHolding.GetComponent<Rigidbody2D>().simulated = false;
+            }
+        }
+    }
+}
